e05ab2576096781dcf2aee71e79e045a614a29ab
Giuseppe Genovese <ggenovese141@gmail.com>
2023-02-23 08:49:18 +0100

    Tutto-Primo commit

---
+++ src/clientserverudp/Studente.java
@@ -0,0 +1,51 @@
+package clientserverudp;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Giuseppe Genovese
+ */
+
+public class Studente implements Serializable {
+    private static final long serialVersionUID = 1;
+    public static final String BLUE = "\u001B[34m";
+    public static final String RESET = "\u001B[0m";
+    private int id;
+    private String name;
+    private String email;
+
+    public Studente(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return "Id = " + BLUE + getId() + RESET + " Nome e Cognome = " + BLUE + getName() + RESET + " Email = " + BLUE + getEmail() + RESET;
+    }
+}
